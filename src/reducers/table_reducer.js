4bb9302a8d39c15bfac1629a2bed1c3eb8c53c5a
Patrick Kennedy <patrickjk94@gmail.com>
2018-07-30 11:53:57 -0400

    fixed table-reducer.

--- src/reducers/table_reducer.js
+++ src/reducers/table_reducer.js
@@ -5,7 +5,7 @@ const table_reducer = (state = { has_data: false, rows: [] }, action) =>
   switch(action.type)
   {
     case "DELETE_USER": 
-      let newArray = state.slice();
+      let newArray = state.rows.slice();
       var i;
       for (i = 0; i < newArray.length; i++)
       {
@@ -15,12 +15,9 @@ const table_reducer = (state = { has_data: false, rows: [] }, action) =>
               break;
           }
       }
-      return newArray; 
+      return {has_data: state.has_data, rows: newArray}; 
     case "ADD_USER": 
-      console.log("ADD_USER"); 
-      return [ ...state, 
-        {id: (state.length+1), fname: action.user.fname, lname: action.user.lname} 
-      ]; 
+      return {has_data: state.has_data, rows: [...state.rows, {id: (state.length + 1), fname: action.user.fname, lname: action.user.lname} ]}; 
     case "TABLE_DATA_LOADED": 
         let new_state = {}; 
         new_state.has_data = ! state.has_data; 
